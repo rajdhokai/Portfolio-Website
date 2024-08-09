6ee9c90cfdd58d93d36f2afa2b96edb5e21c53be
Dhokai Raj B <100281708+rajdhokai@users.noreply.github.com>
2024-08-09 22:41:30 +0530

    initial-commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Raj's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="icon"
+          href="https://st3.depositphotos.com/16030310/18317/v/380/depositphotos_183176596-stock-illustration-letters-logo-initial-logo-identity.jpg"
+          sizes="any"
+        />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
